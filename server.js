ca753d4d25d662c3f147300adf257c3054124754
Arnold Lei <arnold.lei88@gmail.com>
2016-07-27 15:26:33 -0400

    added msql database and table

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var questions;
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'friendFinder'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM questions', function(err, rows, fields) {
+  if (err) throw err;
+ 
+  questions = rows;
+});
+
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
@@ -12,6 +29,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.render('index');
+});
+
+app.get('/survey', function(req, res) {
+    res.render('survey', questions);
 });
 
 var port = 3000;
